aeb23e482bdc202cf3dcdf32084a5230e5f2aa4a
djatiaja <djamgt23@gmail.com>
2024-06-11 03:22:02 +0700

    img for tempatWisata done

--- website/model.py
+++ website/model.py
@@ -1,20 +1,21 @@
 from . import db
 from flask_login import UserMixin
 class TempatWisata(db.Model):
-    __tablename__ = 'tempatWisata'
-    Place_Id = db.Column(db.BigInteger, primary_key=True)
-    Place_Name = db.Column(db.String(1024), nullable=False)
-    Description = db.Column(db.String(1024))
-    Category = db.Column(db.String(1024))
-    City = db.Column(db.String(1024))
-    Price = db.Column(db.BigInteger)
-    Rating = db.Column(db.Float)
-    Time_Minutes = db.Column(db.String(1024), nullable=True)
-    Coordinate = db.Column(db.String(1024))
-    Lat = db.Column(db.Float)
-    Long = db.Column(db.Float)
+    __tablename__ = 'tempatwisata'
+    
+    Place_Name = db.Column(db.String(512), nullable=False)
+    Description = db.Column(db.String(512), nullable=False)
+    Category = db.Column(db.String(512), nullable=False)
+    City = db.Column(db.String(512), nullable=False)
+    Price = db.Column(db.Integer, nullable=False)
+    Rating = db.Column(db.Float, nullable=False)
+    Time_Minutes = db.Column(db.String(512), nullable=False)
+    Lat = db.Column(db.Float, nullable=False)
+    Long = db.Column(db.Float, nullable=False)
+    Place_Id = db.Column(db.Integer, primary_key=True, nullable=False)
+    Image = db.Column(db.String(512), nullable=False)
     def __repr__(self):
-        return f"{{'Place_Id': {self.Place_Id}, 'Place_Name': '{self.Place_Name}', 'Description': '{self.Description}', 'Category': '{self.Category}', 'City': '{self.City}', 'Price': {self.Price}, 'Rating': {self.Rating}, 'Time_Minutes': '{self.Time_Minutes}', 'Coordinate': '{self.Coordinate}', 'Lat': {self.Lat}, 'Long': {self.Long}}}"
+        return f"{{'Place_Name': '{self.Place_Name}', 'Description': '{self.Description}', 'Category': '{self.Category}', 'City': '{self.City}', 'Price': {self.Price}, 'Rating': {self.Rating}, 'Time_Minutes': '{self.Time_Minutes}', 'Lat': {self.Lat}, 'Long': {self.Long}, 'Place_Id': {self.Place_Id}, 'Image': '{self.Image}'}}"
 
 class User(UserMixin, db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
